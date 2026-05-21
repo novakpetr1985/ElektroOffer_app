7f4b9667d54ff5f28afdd24208776c1198ebe967
novakpetr1985 <novakpetr1985@gmail.com>
2026-05-21 22:42:23 +0200

    save

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ElektroOffer_app.Models;
+
+namespace ElektroOffer_app.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<WorkItem> WorkItems => Set<WorkItem>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=elektrooffer.db");
+        }
+    }
+}
---
+++ Models/WorkItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ElektroOffer_app.Models
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal PricePerUnit { get; set; }
+
+        public string Unit { get; set; } = "ks";
+    }
+}
