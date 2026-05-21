563cd8c7fef9f2dbd079a3386094a0d23f654a6d
novakpetr1985 <novakpetr1985@gmail.com>
2026-05-21 22:46:52 +0200

    OK

--- obj/Debug/net10.0-windows/ElektroOffer_app.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ElektroOffer_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4b9667d54ff5f28afdd24208776c1198ebe967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d40fc1af760aa8d66bb7ede5a9bc48f1ed21b7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
