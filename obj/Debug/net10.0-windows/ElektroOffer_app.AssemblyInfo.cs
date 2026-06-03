800f4d73100a5d7ac7bba515db552267cd5dbbca
novakpetr1985 <novakpetr1985@gmail.com>
2026-06-03 22:11:10 +0200

    alignement to right
    Zarovnání doprava součtu PRÁCE a MATERIÁL a celkový součet

--- obj/Debug/net10.0-windows/ElektroOffer_app.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ElektroOffer_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9467275f80ac69776e6e4bf8c7625fe745f3379f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077154ba4f8cf91b5126133e089880b4c55cd231")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElektroOffer_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
